0fc2ccfcebc984d922ceeded90205f3007db17ba
Mayank Mehta <mayankmehta880@gmail.com>
2025-04-05 18:56:06 +0530

    nested loop

--- first.py
+++ first.py
@@ -3,5 +3,12 @@ height = int(input("What is your height?"))
 
 if height > 120 :
     print("You are elibile")
+    age = int(input("What is your age? "))
+    if age <= 18:
+        print("ticket is 7Rs")
+    elif 18 < age  < 22:
+        print("ticket is 12Rs")
+    else :
+        print("ticket is 15Rs")    
 else :
     print("NO")
