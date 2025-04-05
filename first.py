4a42a8a9f431444e55373c75b76539c5e31606a9
Mayank Mehta <mayankmehta880@gmail.com>
2025-04-05 16:22:31 +0530

    commit

--- first.py
+++ first.py
@@ -1,4 +1,7 @@
-city = input("What city you live in? \n")
-pet = input("what is your pet name? \n")
+bill = input("What was the total bill? $ ")
+tip = input("How much tip would you like to give? 10, 12, or 15?")
+people = input("How many people to split the bill? ")
 
-print("Your brand name could be " + city + " "+ pet)
+totalAmt = float(bill) + float(tip)
+eachPay = totalAmt / float(people)
+print("Each person should pay: $ " , float(eachPay))
