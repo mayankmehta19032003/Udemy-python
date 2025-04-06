ecd6a0c2f8e0d44cc46806007c7dc0212302659a
Mayank Mehta <mayankmehta880@gmail.com>
2025-04-06 19:40:38 +0530

    max number

---
+++ day5.py
@@ -0,0 +1,10 @@
+# students_scores = [122,22,223,202,98]
+
+# maxi = 0
+# for score in students_scores:
+#     if score > maxi:
+#         maxi = score
+# print(maxi)
+
+for number in range(1,11):  #does not include last argument
+    print(number)
