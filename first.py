07e8cd7980ea9152947cf888d9839435fec475d0
Mayank Mehta <mayankmehta880@gmail.com>
2025-04-06 15:36:38 +0530

    commit

--- first.py
+++ first.py
@@ -1,27 +1,44 @@
-print("Welcome to Pizza shop")
+#randomisation
+import random
 
-size = input("What size of pizza S , M or L ? ")
-pepproni = input("Want pepproni pizza? Y or N: ")
-extra_cheese = input("want extra cheese? Y or N: ")
-bill =0
+print(random.randint(1,20))
 
-if size == "S":
-    bill = 15
-    if pepproni == "Y":
-        bill += 2
-    if extra_cheese == "Y":
-        bill += 1
-elif size == "M":
-    bill = 20
-    if pepproni == "Y":
-        bill += 3
-    if extra_cheese == "Y":
-        bill += 1
-else :
-    bill = 25
-    if pepproni == "Y":
-        bill += 3
-    if extra_cheese == "Y":
-        bill += 1
 
-print(f"Your total bill is: {bill}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#  print("Welcome to Pizza shop")
+
+# size = input("What size of pizza S , M or L ? ")
+# pepproni = input("Want pepproni pizza? Y or N: ")
+# extra_cheese = input("want extra cheese? Y or N: ")
+# bill =0
+
+# if size == "S":
+#     bill = 15  
+# elif size == "M":
+#     bill = 20 
+# else :
+#     bill = 25
+
+# if pepproni == "Y":
+#     bill +=
+
+# print(f"Your total bill is: {bill}")
+
