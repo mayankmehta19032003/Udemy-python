09ea20f4e775d06a1670f33a9a75a21f472789ea
Mayank Mehta <mayankmehta880@gmail.com>
2025-04-09 16:06:35 +0530

    implement the decode and encode func

--- day8_Caesar_Chiper.py
+++ day8_Caesar_Chiper.py
@@ -6,3 +6,19 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower
 text = input("Type your message:\n").lower()
 
 shift = int(input("Type the shift number:\n"))
+
+encode = ""
+decode = ""
+
+if direction == "encode":
+    for letter in text:
+        index = alphabets.index(letter)
+        encode = encode + alphabets[(index+shift) % 26]
+    print(encode)
+elif direction == "decode":
+    for letter in text:
+        index = alphabets.index(letter)
+        decode = decode + alphabets[(index-shift) % 26]
+    print(decode)
+else:
+    print("Enter valid direction")
