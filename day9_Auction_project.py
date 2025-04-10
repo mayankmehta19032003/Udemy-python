79dd5a99463bd4fcd3279ed3aa1a2371073e06ea
Mayank Mehta <mayankmehta880@gmail.com>
2025-04-10 13:52:09 +0530

    completed

--- day9_Auction_project.py
+++ day9_Auction_project.py
@@ -1,8 +1,39 @@
-dic = {
-    "name": "mayank",
-    "age": 22,
-    "location": "Kota",
-    123:"example for number key"
-}
+print('''
+___________
+ /          \\
+ )_______(
+ |"""""""|_.-._,.---------.,_.-._
+ |       | | |               | | ''-.
+ |       |_| |_             _| |_..-'
+ |_______| '-' ''' + "---------" + ''' ' '-'
+ )"""""""(
+/_________\\
+.-------------.
+/_______________\\''')
 
-print(dic[123])
+
+should_continue = True
+dic = {}
+
+while should_continue == True:
+    name = input("What is your name?: ")
+    bid = int(input("What is your bid?: "))
+    dic[name] = bid
+
+    choice = input("Are there any other bidders? Type 'yes or 'no'. ").lower()
+    if choice == "yes":
+        print("\n" * 50)
+        continue
+    elif choice == "no":
+        maxi = 0
+        winner = ""
+        for name,bid in dic.items():
+            if bid > maxi:
+                maxi = bid
+                winner = name
+        print(f"The winner is {winner} with a bid of {maxi}")
+        should_continue = False
+    else:
+        print("Choice should be in yes or no")
+        should_continue = False
+
