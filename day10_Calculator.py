dd349811234052f06a4935e4e5228c6e4a4b724f
Mayank Mehta <mayankmehta880@gmail.com>
2025-04-11 15:49:54 +0530

    done project

--- day10_Calculator.py
+++ day10_Calculator.py
@@ -1,3 +1,6 @@
+import os
+
+
 def add(n1,n2):
     return n1+n2
 
@@ -18,4 +21,29 @@ operations = {
     "/": divide,
 }
 
-print(operations["*"](4,8))
+def calculator():
+    should_accumulate = True
+    first_number = float(input("What is the first number? "))
+
+    while should_accumulate:
+        for symbol in operations:
+            print(symbol)
+
+        operation_symbol = input("Pick an operation: ")
+        next_number = float(input("What is the next number? "))
+
+        result = operations[operation_symbol](first_number, next_number)
+        print(f"{first_number} {operation_symbol} {next_number} = {result}")
+
+        choice = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
+
+        if choice == "y":
+            first_number = result
+        else:
+            should_accumulate = False
+            os.system('cls' if os.name == 'nt' else 'clear')
+            calculator()
+
+
+calculator()
+    
