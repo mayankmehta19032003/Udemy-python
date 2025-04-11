ffeb133468ae7fd35a7232215b4079479115c12c
Mayank Mehta <mayankmehta880@gmail.com>
2025-04-11 14:37:17 +0530

    use title()

--- day10_Calculator.py
+++ day10_Calculator.py
@@ -1,1 +1,8 @@
 # functions with output
+def format_name(f_name,l_name):
+    first_name = f_name.title()
+    last_name = l_name.title()
+
+    print(first_name + last_name)
+
+format_name("maYANk","MEhta")
