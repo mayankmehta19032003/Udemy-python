064ec94485c81b9395b9a4464a7cc22e06abb31a
Mayank Mehta <mayankmehta880@gmail.com>
2025-04-13 18:26:57 +0530

    build structure

--- day13.py
+++ day13.py
@@ -1,3 +1,5 @@
+from day13_data import data
+
 print('''__  ___       __             
         / / / (_)___ _/ /_  ___  _____
        / /_/ / / __ '/ __ \/ _ \/ ___/
@@ -7,3 +9,18 @@ print('''__  ___       __
       / /   / __ \ | /| / / _ \/ ___/
      / /___/ /_/ / |/ |/ /  __/ /    
     /_____/\____/|__/|__/\___/_/ ''')
+
+print(f"Compare A: {data[0]['name']}, {data[0]['description']}, {data[0]['country']} ")
+
+print(''' 
+          _    __    
+         | |  / /____
+         | | / / ___/
+         | |/ (__  ) 
+         |___/____(_)''')
+
+print(f"Against B: {data[1]['name']}, {data[1]['description']}, {data[1]['country']}")
+
+choice = input("Who has more followers? Type 'A' or 'B': ")
+
+
