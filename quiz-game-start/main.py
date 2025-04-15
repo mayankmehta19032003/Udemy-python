dc585e209d1cf22782b5fe88b807aba835be24b6
Mayank Mehta <mayankmehta880@gmail.com>
2025-04-15 16:58:30 +0530

    quiz brain completed

--- quiz-game-start/main.py
+++ quiz-game-start/main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 
 question_bank = []
@@ -7,3 +8,7 @@ question_bank = []
 for i in question_data:
    question_bank.append(Question(i["text"], i["answer"]))
 
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_question():
+    quiz.next_question()
