c328c9da32cab705ed5807f850ec9163493f5d4c
Mayank Mehta <mayankmehta880@gmail.com>
2025-04-18 02:51:31 +0530

    max el in list

--- day-25/main.py
+++ day-25/main.py
@@ -1,10 +1,23 @@
-import csv
+# import csv
 
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    next(data)
-    temperature =[]
-    for row in data:
-        temp = int(row[1])
-        temperature.append(temp)
-    print(temperature)
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     next(data)
+#     temperature =[]
+#     for row in data:
+#         temp = int(row[1])
+#         temperature.append(temp)
+#     print(temperature)
+# import pandas
+
+# data = pandas.read_csv("weather_data.csv")
+
+# data_dict = data.to_dict()
+# # print(data_dict)
+
+# temp_list = data["temp"].to_list()
+# # average =sum(temp_list)/len(temp_list)
+
+# # print(average)
+
+# print(data["temp"].max())
