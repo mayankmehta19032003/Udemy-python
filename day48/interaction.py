a51f423a5409c428f3089dd4786425e4055c6414
Mayank Mehta <mayankmehta880@gmail.com>
2025-04-25 23:43:40 +0530

    input and enter key

--- day48/interaction.py
+++ day48/interaction.py
@@ -8,10 +8,16 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
-element = driver.find_element(By.CSS_SELECTOR, 'a[title="Special:Statistics"]')
-print(element.text)
+# element = driver.find_element(By.CSS_SELECTOR, 'a[title="Special:Statistics"]')
+element = driver.find_element(By.LINK_TEXT,value="Content portals")
+# element.click()
+
+# find search abr and type
+search = driver.find_element(By.NAME,value="search")
+search.send_keys("Mayank")
 
 
 
 
-driver.quit()
+
+# driver.quit()
