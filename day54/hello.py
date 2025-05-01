e5e52ae0a60f5a945d2cb9031bf41846f9c8e825
Mayank Mehta <mayankmehta880@gmail.com>
2025-05-01 17:17:48 +0530

    dynamic route

--- day54/hello.py
+++ day54/hello.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/<name>")
+def greet(name):
+    return f"Hello {name}"
+
+if __name__ == "__main__":
+    app.run()
