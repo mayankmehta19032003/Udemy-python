e6e754c3b999ffa330c17fab7828a309b05f36a7
Mayank Mehta <mayankmehta880@gmail.com>
2025-05-15 13:57:11 +0530

    completed challenge

--- day-57-start/main.py
+++ day-57-start/main.py
@@ -1,15 +1,23 @@
 from flask import Flask, render_template
 import random
 from datetime import datetime
+import requests
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def home():
     random_number = random.randint(1,10)
     current_year = datetime.now().year
     return render_template("index.html",num=random_number, year=current_year)
+
+@app.route('/guess/<name>')
+def guess(name):
+    response =requests.get(f"https://api.genderize.io/?name={name}")
+    response = response.json()
+    gender = response['gender']
+    return render_template("guess.html",name=name,gender=gender)
+
 
 
 if __name__ == "__main__":
